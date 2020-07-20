d5dccd1a55e47a1dc1ca974fe507cc72071ea23b
kimhan0421 <kimhan0421@naver.com>
2020-07-20 23:38:41 +0900

    create likelion page

--- proj/blog/views.py
+++ proj/blog/views.py
@@ -7,4 +7,12 @@ def home(request):
 
 def detail(request, blog_id):
     blog_detail = get_object_or_404(Blog, pk=blog_id)
-    return render(request, 'blog/detail.html', {'blog': blog_detail})
+    return render(request, 'detail.html', {'blog': blog_detail})
+
+def likelion(request):
+    return render(request, 'likelion.html', {'likelion': likelion})
+
+def like2019(request):
+    return render(request, 'like2019.html', {'like2019': like2019})
+def like2020(request):
+    return render(request, 'like2020.html', {'like2020': like2020})
--- proj/blog_pro2/urls.py
+++ proj/blog_pro2/urls.py
@@ -5,5 +5,8 @@ import blog.views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', blog.views.home, name="home"),
+    path('/likelion', blog.views.likelion, name="likelion"),
     path('blog/<int:blog_id>/', blog.views.detail, name="detail"),
+    path('/like2019', blog.views.like2019, name="like2019"),
+    path('/like2020', blog.views.like2019, name="like2020"),
 ]
